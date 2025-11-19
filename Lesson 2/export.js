ec0aaa44257d36bc529a1a0f6e088418e7c17011
Himanshu sharma <himanshusharmapune@gmail.com>
2025-11-19 09:01:28 +0530

    Sample

--- Lesson 2/export.js
+++ Lesson 2/export.js
@@ -1,2 +1,21 @@
+ 
+ /*
+//these exports are called named exports
 export const a = 10;
 export const b = 30;
+
+//this is a default export and can be imported by any name(check main.js)
+const obj ={
+    x:5,
+    y:5
+}
+export default obj;
+ */
+
+
+//if you want to use require syntax then we have to remove type:"module" from package.json
+module.exports ={
+    a:1,
+    b:4,
+    c:5
+}
