b26023434f9fb1643733ab6b3bb2b9f86152a6f4
Himanshu sharma <himanshusharmapune@gmail.com>
2025-11-19 08:10:14 +0530

    Sample

---
+++ Lesson 2/main.js
@@ -0,0 +1,20 @@
+//const { createServer } = require('node:http');
+import http from "http"
+import {a} from "./export.js"
+console.log(a)
+
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Himanshu sharma</h1>');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});  
+  
