48559798bdbbedf931ac69ff6bcff17ebeb298e6
Himanshu sharma <himanshusharmapune@gmail.com>
2025-11-27 10:30:31 +0530

    Express

--- Lesson 4/main.js
+++ Lesson 4/main.js
@@ -1,11 +1,40 @@
-const http = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World22')
+})
+
+/* 
+//
+app.get('/about', (req, res) => {
+  res.send('About us')
+})
+
+app.get('/contact', (req, res) => {
+  res.send('Contact')
+})
+app.get('/blog', (req, res) => {
+  res.send('Blog')
+}) 
+app.get('/blog/learning-web', (req, res) => {
+  res.send('Web dev data')
+})
+*/
+
+app.get('/blog', (req, res) => {
+  res.send('Blog')
+}) 
+
+app.get('/blog/:slug', (req, res) => {
+  console.log(req.params) // will output { slug: 'web' }
+   console.log(req.query) // will output { mode: 'dark', region: 'in' }
+  res.send(`This is intro to ${req.params.slug}`)
+}) 
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
