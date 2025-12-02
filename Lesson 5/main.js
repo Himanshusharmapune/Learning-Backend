8a98efe4df3622ddb6d442f5c5f38a45e5efe40b
Himanshu sharma <himanshusharmapune@gmail.com>
2025-12-02 14:08:13 +0530

    Response, Request and Routers in Express

--- Lesson 5/main.js
+++ Lesson 5/main.js
@@ -1,19 +1,33 @@
 const express = require('express')
+const blog = require('./routes/blog')
+const shop = require('./routes/shop')
 const app = express()
 const port = 3000
+
 
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
   console.log("Get is working!!")
-  res.send('Hello World!')
-}) 
-
-app.post('/', (req, res) => {
+  res.send('Hello World0!')
+}).post('/', (req, res) => {
   console.log("Post is working!!")
-  res.send('Hello World!')
+  res.send('Hello World1!')
+}).put('/', (req, res) => {
+  console.log("put is working!!")
+  res.send('Hello World! put')
+})
+
+app.get("/index", (req,res)=>{
+  console.log("Hey its index")
+  res.sendFile('templates/index.html',{root:__dirname})
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+//blog starts
+app.use('/blog',blog)
+app.use('/shop',shop)
