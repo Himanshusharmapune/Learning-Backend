4384e99cc567a3d1ba92cc323868edbda3060cd2
Himanshu sharma <himanshusharmapune@gmail.com>
2025-12-06 21:33:40 +0530

    Leaning node

--- Lesson 6/main.js
+++ Lesson 6/main.js
@@ -1,11 +1,19 @@
-const fs = require("fs");
+//const fs = require("fs");
 // fs.writeFile("message.txt","Hello Node",(err)=>{
 //   if(err) throw err;
 //   console.log("The file has been saved")
 // });
 
-fs.readFile("message.txt", "utf8", (err,data)=>{
-  if(err) throw err;
-  console.log(data)
-})
+// fs.readFile("message.txt", "utf8", (err,data)=>{
+//   if(err) throw err;
+//   console.log(data)
+// })
 
+
+// var generateName =  require("sillyname");
+// var sillyname = generateName();
+// console.log(`My Name is ${sillyname}`);
+
+import superheroes from "superheroes"
+const name = superheroes.random();
+console.log(`I am ${name}`)
