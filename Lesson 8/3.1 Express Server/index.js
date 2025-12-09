0af1e53e0387217400f853b4faa14771d167d903
Himanshu sharma <himanshusharmapune@gmail.com>
2025-12-09 22:22:17 +0530

    Express

--- Lesson 8/3.1 Express Server/index.js
+++ Lesson 8/3.1 Express Server/index.js
@@ -1,6 +1,16 @@
 import express from "express";
 const app = express();
 const port = 3000;
+
+app.get("/",(req,res)=>{
+   res.send("Hello world")
+})
+
+app.get("/about",(req,res)=>{
+   res.send("<h1>About me</h1>")
+})
+
+
 app.listen(port,()=>{
-    console.log("Server running on port 3000");
+    console.log(`Server running on port ${port}`);
 })  
